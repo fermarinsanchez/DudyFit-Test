a5d38d3aff97e057400f650fc445b4013e3f7900
fermarinsanchez <fermarinsanchez@yahoo.es>
2021-01-01 21:57:45 +0100

    feat: all iterations done except the last

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,2 +1,15 @@
-// const hbs = require('hbs')
-// const path = require('path')
+const hbs = require('hbs')
+
+
+hbs.registerHelper('matched', (trainers, clients) => {
+    let sortTrainers = trainers.trainers.sort((a,b) => a.reputation - b.reputation)
+    let sortClients = clients.clients.sort((a,b) => a.trainerRep - b.trainerRep)
+    let result = [...sortTrainers]
+    for(let i = 0; i < sortTrainers.length; i++) {
+     if (sortTrainers[i].available !== 0) {
+      const cli = sortClients.splice(0, sortTrainers[i].available)
+      sortTrainers[i].clients = cli
+     }
+    }
+    return result
+  })
